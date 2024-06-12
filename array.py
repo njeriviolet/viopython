f3c3278bb8e1dd4c3f2814f8a69f8fdd3b20662c
Chrismeso <njeriviolet01@gmail.com>
2024-06-12 12:09:41 +0300

    progress

---
+++ array.py
@@ -0,0 +1,17 @@
+courses=["Computer Science","IT","BBIT","Software engineering"]
+print (courses)
+
+#accessing an element in an array
+print(courses[2])
+
+#looping through an array
+for x in courses : print  (x)
+
+#adding a new element in an array
+courses.append("data science")
+
+#deleting an elemnt in an array
+
+courses.remove("IT")
+print(courses)
+
---
+++ string.py
@@ -0,0 +1,25 @@
+greeting = "hello world"
+
+device ="COMPUTER"
+
+print (greeting)
+
+#accessing an element in a string
+
+print(greeting[0])
+print(greeting[1])
+
+#size/length of the string
+print(len(greeting))
+
+#modyfying a string
+print(greeting.upper())
+print(device.lower())
+
+#String concatenation -joining words
+firstname= "violet"
+lastname=  "njeri"
+print( firstname +  " " + "lastname")
+
+
+
